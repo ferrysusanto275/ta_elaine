3c332dd5aa682108afe3f94312b6c9e723eac29b
Ferry Susanto <ferrysusanto275@gmail.com>
2023-10-16 04:51:21 +0700

    aspe domain model

---
+++ app/models/aspek.py
@@ -0,0 +1,62 @@
+from app.utils.database import db
+from datetime import datetime
+
+class aspekModel:
+    table_name="aspek"
+    prefix="i"
+    def getAll(self):
+        query="SELECT * FROM "+self.table_name;
+        cur= db.execute_query(query)
+        result=cur.fetchall()
+        data=[]
+        for row in result:
+            data.append({"id":row[0],"nama":row[1],"bobot":row[2]})
+        cur.close()
+        return data
+    def getById(self,id):
+        query="SELECT * FROM "+self.table_name;
+        query+=" WHERE id=%s"
+        cur= db.execute_query(query,(id,))
+        result=cur.fetchone()
+        data=result
+        if(result):
+            data={"id":result[0],"name":result[1],"bobot":result[2]}
+        cur.close()
+        return data
+    def getLastId(self,code):
+        code_q=code+"%"
+        query="SELECT MAX(id) FROM "+self.table_name
+        query+=" WHERE id LIKE %s"
+        cur= db.execute_query(query,(code_q,))
+        result=cur.fetchone()
+        idx=0
+        if(result[0] is not None):
+            idx=int(result[0][-5:])
+        idx+=1;
+        strIdx="00000"+str(idx)
+        strIdx=strIdx[-5:]
+        cur.close()
+        return code+strIdx
+    def create(self,nama,bobot):
+        current_date = datetime.now().date()
+        code=self.prefix+current_date.strftime("%Y%m%d")
+        query="INSERT INTO "+self.table_name
+        query+=" (id, nama,bobot)"
+        query+=" VALUES (%s, %s,%s)"
+        cur=db.execute_query(query,(self.getLastId(code),nama,bobot))
+        db.commit()
+        cur.close()
+        return True
+    def update(self,nama,bobot,id):
+        query="UPDATE "+self.table_name
+        query+=" SET nama=%s, bobot=%s"
+        query+=" WHERE id=%s"
+        db.execute_query(query,(nama,id))
+        db.commit()
+        return True
+    def delete(self,id):
+        query="DELETE FROM "+self.table_name
+        query+=" WHERE id=%s"
+        db.execute_query(query,(id,))
+        db.commit()
+        return True
