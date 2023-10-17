b5c55dacec0f98a5dd61c8765ba275e85840f3e3
Ferry Susanto <ferrysusanto275@gmail.com>
2023-10-17 23:36:33 +0700

    upate db

--- elaine_ta.sql
+++ elaine_ta.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 16, 2023 at 09:03 PM
+-- Generation Time: Oct 17, 2023 at 06:36 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 5.6.40
 
@@ -35,13 +35,6 @@ CREATE TABLE `aspek` (
   `bobot` decimal(11,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `aspek`
---
-
-INSERT INTO `aspek` (`id`, `domain`, `nama`, `bobot`) VALUES
-('a2023101700002', 'd2023101700001', 'coba aspek baru', '20.00');
-
 -- --------------------------------------------------------
 
 --
@@ -53,13 +46,6 @@ CREATE TABLE `domain` (
   `nama` varchar(255) NOT NULL,
   `bobot` decimal(11,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `domain`
---
-
-INSERT INTO `domain` (`id`, `nama`, `bobot`) VALUES
-('d2023101700001', 'coba domain ganti', '5.00');
 
 -- --------------------------------------------------------
 
@@ -74,13 +60,6 @@ CREATE TABLE `indikator` (
   `bobot` decimal(11,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `indikator`
---
-
-INSERT INTO `indikator` (`id`, `aspek`, `nama`, `bobot`) VALUES
-('in2023101700001', 'a2023101700002', 'coba indikator', '15.00');
-
 -- --------------------------------------------------------
 
 --
@@ -92,14 +71,6 @@ CREATE TABLE `instansi` (
   `nama` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `instansi`
---
-
-INSERT INTO `instansi` (`id`, `nama`) VALUES
-('i2023101700001', 'coba'),
-('i2023101700002', 'coba 2');
-
 -- --------------------------------------------------------
 
 --
@@ -109,15 +80,9 @@ INSERT INTO `instansi` (`id`, `nama`) VALUES
 CREATE TABLE `isi` (
   `instansi` varchar(25) NOT NULL,
   `indikator` varchar(25) NOT NULL,
+  `year` year(4) NOT NULL,
   `value` decimal(11,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `isi`
---
-
-INSERT INTO `isi` (`instansi`, `indikator`, `value`) VALUES
-('i2023101700002', 'in2023101700001', '8.00');
 
 --
 -- Indexes for dumped tables
@@ -151,7 +116,7 @@ ALTER TABLE `instansi`
 -- Indexes for table `isi`
 --
 ALTER TABLE `isi`
-  ADD PRIMARY KEY (`instansi`,`indikator`);
+  ADD PRIMARY KEY (`instansi`,`indikator`,`year`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
