2c74a239f332019c1591761d0b5c4e4edc5a694c
ElaineAndrianaHosni <6182001045@student.unpar.ac.id>
2023-10-26 21:39:50 +0700

    api done +grup read

---
+++ app/controllers/grup_instansi_controller.py
@@ -0,0 +1,50 @@
+from flask import Blueprint,jsonify,request
+from app.models.grup_instansi import grup_instansiModel
+model=grup_instansiModel()
+def validasiInput():
+    nama = request.json.get('nama')
+    if not nama:
+        return jsonify({'message': 'Nama is required'}), 400
+    return [nama]
+grup_instansi_bp=Blueprint(model.table_name,__name__)
+@grup_instansi_bp.route('/api/'+model.table_name)
+def get_all():
+    return jsonify(model.getAll());
+@grup_instansi_bp.route('/api/'+model.table_name+'/<string:id>')
+def get_by_id(id):
+    grup_instansi = model.getById(id)
+    if grup_instansi:
+        return jsonify(grup_instansi)
+    else:
+        return jsonify({'message': model.table_name.capitalize()+' not found'}), 404
+@grup_instansi_bp.route('/api/'+model.table_name,methods=['POST'])
+def create():
+    data=validasiInput()
+    if not isinstance(data,list):return data
+    if model.create(data[0]):
+        return jsonify({'message': model.table_name.capitalize()+' created'}), 201
+    else:
+        return jsonify({'message': 'Failed to create '+model.table_name}), 500
+@grup_instansi_bp.route('/api/'+model.table_name+'/<string:id>', methods=['PUT'])
+def update(id):
+    data=validasiInput()
+    if not isinstance(data,list):return data
+    grup_instansi = model.getById(id)
+    if grup_instansi:
+        if model.update( data[0],id):
+            return jsonify({'message': model.table_name.capitalize()+' updated'})
+        else:
+            return jsonify({'message': 'Failed to update '+model.table_name}), 500
+    else:
+        return jsonify({'message': model.table_name.capitalize()+' not found'}), 404
+
+@grup_instansi_bp.route('/api/'+model.table_name+'/<string:id>', methods=['DELETE'])
+def delete_user(id):
+    grup_instansi = model.getById(id)
+    if grup_instansi:
+        if model.delete(id):
+            return jsonify({'message': model.table_name.capitalize()+' deleted'})
+        else:
+            return jsonify({'message': 'Failed to delete '+model.table_name}), 500
+    else:
+        return jsonify({'message': model.table_name.capitalize()+' not found'}), 404
