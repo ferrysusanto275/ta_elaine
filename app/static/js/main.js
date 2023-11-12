fe04aa874ff6d796ead87e1e434effcb5897cc99
Ferry Susanto <ferrysusanto275@gmail.com>
2023-11-13 00:49:52 +0700

    on hover drop down

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,11 +1,4 @@
 const base_api_url = "http://127.0.0.1:5000/api/";
-const drop_down_menu = document.querySelectorAll(".drop-down");
-drop_down_menu.forEach((dd) => {
-  dd.onclick = () => {
-    if (dd.classList.contains("active")) dd.classList.remove("active");
-    else dd.classList.add("active");
-  };
-});
 
 const li_menu = document.querySelectorAll("li");
 li_menu.forEach((li) => {
