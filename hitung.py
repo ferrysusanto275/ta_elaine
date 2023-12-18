c8b321910c518256dba3441dd7c41d6664c1963f
Ferry Susanto <ferrysusanto275@gmail.com>
2023-12-18 13:38:04 +0700

    masukin data part 09 masih error

--- hitung.py
+++ hitung.py
@@ -4,7 +4,7 @@ from app.models.isi import isiModel
 from scipy.optimize import minimize
 
 model=isiModel()
-df = pd.read_csv('Data CSV/Data_lengkap_part_08.csv')
+df = pd.read_csv('Data CSV/Data_lengkap_part_09.csv')
 for index, row in df.iterrows():
     if(row.id!='NaN'):
         def objective(params):
