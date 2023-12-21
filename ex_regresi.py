99d5f4ec4050a7d109f851ca1ab58d7404bd8641
ElaineAndrianaHosni <6182001045@student.unpar.ac.id>
2023-12-21 18:53:48 +0700

    data

--- ex_regresi.py
+++ ex_regresi.py
@@ -6,7 +6,8 @@ from app.models.indikator import indikatorModel
 
 model=isiModel()
 # indikator_model=indikatorModel();
-df = pd.read_csv('Data CSV/Data_lengkap_part_13.csv')
+# df = pd.read_csv('Data CSV/Data_lengkap_part_01.csv')
+df=pd.DataFrame({'id':['i2023110600313'],'indeks_2018':[2.09],'indeks_2019':[1.89],'indeks_2020':[2.57]})
 # list_indikator=indikator_model.getAll()
 def objective(params):
     i1,i2,i3,i4,i5,i6,i7,i8,i9,i10,i11,i12,i13,i14,i15,i16,i17,i18,i19,i20,i21,i22,i23,i24,i25,i26,i27,i28,i29,i30,i31,i32,i33,i34,i35,i36,i37,i38,i39,i40,i41,i42,i43,i44,i45,i46,i47=params
@@ -23,162 +24,138 @@ def objective(params):
     domain3=(aspek5*(12/16.5))+(aspek6*(4.5/16.5))
     domain4=(aspek7*(27.5/45.5))+(aspek8*(18/45.5))
     return (domain1*(13/100))+(domain2*(25/100))+(domain3*(16.5/100))+(domain4*(45.5/100))
+def cari_i(index_find,index1,indikators_1,indikators_2):
+    data=[]
+    pilihan_data=[];
+    return data
 for index, row in df.iterrows():
     if(row.id!='NaN'):
         data_full = {"2018": [], "2019": [], "2020": [], "2021": [], "2022": []}
-        # esktrapolasi index (regresi)
-        # search index 2021
+#         # esktrapolasi index (regresi)
+#         # search index 2021
         index_2021=model.getIndexbyYearInstansi(instansi=row.id,year=2021);
-        # search index 2022
+#         # search index 2022
         index_2022=model.getIndexbyYearInstansi(instansi=row.id,year=2022);
-        # jika data 2021 dan 2022 di temukan
-        if(index_2021>0 and index_2022 >0):
-            X_train = np.array([index_2021,index_2022]).reshape(-1, 1)
-        elif(index_2021>0):
-            X_train = np.array([index_2021,index_2021]).reshape(-1, 1)
-        elif(index_2022>0):
-            X_train = np.array([index_2022,index_2022]).reshape(-1, 1)
-        else: X_train=None
-        if(X_train is not None):
-            data_indikator_2021=pd.DataFrame(model.getAllValueByYearInstansi(instansi=row.id,year=2021));
-            data_indikator_2022=pd.DataFrame(model.getAllValueByYearInstansi(instansi=row.id,year=2022));
-            for index_in, row_in in data_indikator_2021.iterrows():
-                indikator_2021=row_in.val
-                row_2022=data_indikator_2022.loc[index_in];
-                indikator_2022=row_2022.val
-                if(indikator_2021==0):indikator_2021=indikator_2022
-                if(indikator_2022==0):indikator_2022=indikator_2021
-                data_full['2021'].append(indikator_2021)
-                data_full['2022'].append(indikator_2022)
-                y_train = np.array([indikator_2021,indikator_2022])
-                        # Melatih model regresi linier
-                model_regresi = LinearRegression()
-                model_regresi.fit(X_train, y_train)
-                # # Membuat prediksi untuk data di luar rentang pelatihan
-                index_2018=0
-                if(float(row.indeks_2018)>0):index_2018=row.indeks_2018
-                index_2019=0
-                if(float(row.indeks_2019)>0):index_2018=row.indeks_2019
-                index_2020=0
-                if(float(row.indeks_2020)>0):index_2018=row.indeks_2020
-                X_extrapolate = np.array([index_2018,index_2019,index_2020]).reshape(-1, 1)
-                y_pred_extrapolate = model_regresi.predict(X_extrapolate)
-                # Modifikasi hasil prediksi menjadi bilangan bulat dalam rentang 0-5
-                y_pred_extrapolate_int = np.clip(np.round(y_pred_extrapolate), 0, 5)
-                data_full['2018'].append(y_pred_extrapolate_int[0])
-                data_full['2019'].append(y_pred_extrapolate_int[1])
-                data_full['2020'].append(y_pred_extrapolate_int[2])
-            # print(len(data_full["2018"]))
-                # regresi menurut indikator
-            for i in range(len(data_full["2018"])):
-                # indikator ke i sebagai penentu
-                for j in range(len(data_full["2018"])):
-                    
-                        if(j==0):
-                            # inisialisasi
-                            data_full['2018_'+str(i)]=[]
-                            data_full['2019_'+str(i)]=[]
-                            data_full['2020_'+str(i)]=[]
-                        X_train=np.array([data_full["2021"][i],data_full["2022"][i]]).reshape(-1,1)
-                        y_train=np.array([data_full["2021"][j],data_full["2022"][j]])
-                        model_regresi = LinearRegression()
-                        model_regresi.fit(X_train, y_train)
-                        X_extrapolate = np.array([data_full["2018"][i],data_full["2019"][i],data_full["2020"][i]]).reshape(-1,1)
-                        y_pred_extrapolate = model_regresi.predict(X_extrapolate)
-                        data_full['2018_'+str(i)].append(y_pred_extrapolate_int[0])
-                        data_full['2019_'+str(i)].append(y_pred_extrapolate_int[1])
-                        data_full['2020_'+str(i)].append(y_pred_extrapolate_int[2])
+        data_indikator_2021=model.getAllValueByYearInstansi(instansi=row.id,year=2021);
+        data_indikator_2022=model.getAllValueByYearInstansi(instansi=row.id,year=2022);
+        if(index_2021==0):
+            index_2021=index_2022
+            data_indikator_2021=data_indikator_2022
+        if(index_2022==0):
+            index_2022=index_2021
+            data_indikator_2022=data_indikator_2021
+        index_2018=0
+        if(float(row.indeks_2018)>0):index_2018=row.indeks_2018
+        index_2019=index_2018
+        if(float(row.indeks_2019)>0):index_2019=row.indeks_2019
+        index_2020=index_2019
+        if(float(row.indeks_2020)>0):index_2020=row.indeks_2020
+        print("indeks_real :",index_2018,index_2019,index_2020,index_2021,index_2022)
+        if(index_2021>0 and index_2022>0):
+            cari_i(index_find=index_2020,index1=index_2021)
+            data_2020=[]
+            pilihan_data_2020=[];
+            for i in range(47):
+                indikator_2021=float(data_indikator_2021.loc[i].val)
+                indikator_2022=float(data_indikator_2022.loc[i].val)
+                indikator_2020=[];
+                if(index_2020>index_2021):
+                    #  naik indeks
+                    if(indikator_2021>indikator_2022):
+                        # indikator naik
+                        if(indikator_2021<5):
+                            indikator_2020.append(indikator_2021+1)
+                        else: indikator_2020.append(5)
+                    elif(indikator_2021==indikator_2022):
+                        indikator_2020.append(indikator_2021);
+                        if(indikator_2021<5):
+                            indikator_2020.append(indikator_2021+1);
+                    elif(indikator_2021<indikator_2022):
+                        if(indikator_2021>1):
+                            indikator_2020.append(indikator_2021-1);
+                        else: indikator_2020.append(1);
+                        if(indikator_2021<5):
+                            indikator_2020.append(indikator_2021+1);
+                        else: indikator_2020.append(1);
+                elif(index_2020==index_2021):
+                    indikator_2020.append(indikator_2021);
+                else:
+                    if(indikator_2021<indikator_2022):
+                        # indikator naik
+                        if(indikator_2021>1):
+                            indikator_2020.append(indikator_2021-1);
+                        else: indikator_2020.append(1);
+                    elif(indikator_2021==indikator_2022):
+                        if(indikator_2021>1):
+                            indikator_2020.append(indikator_2021-1);
+                        indikator_2020.append(indikator_2021);
+                    elif(indikator_2021>indikator_2022):
+                        if(indikator_2021>1):
+                            indikator_2020.append(indikator_2021-1);
+                        else: indikator_2020.append(1);
+                        if(indikator_2021<5):
+                            indikator_2020.append(indikator_2021+1);
+                        else: indikator_2020.append(1);
+                data_2020.append(indikator_2020[0])
+                pilihan_data_2020.append([indikator_2020,0])
+            cnt=0
+            while(index_2020>objective(data_2020) and cnt<47):
+                # print(data_2020[cnt])
+                if(len(pilihan_data_2020[cnt][0])>1):
+                    data_2020[cnt]=pilihan_data_2020[cnt][0][1]
+                    if(index_2020<objective(data_2020)):data_2020[cnt]=pilihan_data_2020[cnt][0][0]
+                # print(cnt,data_2020[cnt])
+                cnt+=1
+            indeks_2020=round(objective(data_2020),2)
+            # print(pd.DataFrame(data_2020))
+            # data_2019=[]
+            # pilihan_data_2019=[];
             
-                # temp_2021= data_full["2021"][:i] + data_full["2021"][i + 1:]
-                # temp_2022= data_full["2022"][:i] + data_full["2022"][i + 1:]
-                # temp_2018= data_full["2018"][:i] + data_full["2018"][i + 1:]
-                # temp_2019= data_full["2019"][:i] + data_full["2019"][i + 1:]
-                # temp_2020= data_full["2020"][:i] + data_full["2020"][i + 1:]
-                # X_train=np.array([temp_2021,temp_2022])
-                # y_train=np.array([data_full["2021"][i],data_full["2022"][i]])
-                # model_regresi = LinearRegression()
-                # model_regresi.fit(X_train, y_train)
-                # X_extrapolate = np.array([temp_2018,temp_2019,temp_2020])
-                # y_pred_extrapolate = model_regresi.predict(X_extrapolate)
-                # # inisialisasi
-                # if(i==0):
-                #     data_full['2018_']=[]
-                #     data_full['2019_']=[]
-                #     data_full['2020_']=[]
-                # print(data_full)    
-                # data_full['2018_'].append(y_pred_extrapolate_int[0])
-                # data_full['2019_'].append(y_pred_extrapolate_int[1])
-                # data_full['2020_'].append(y_pred_extrapolate_int[2])
-                # initial_guess_2018.append(y_pred_extrapolate_int[0])
-                # initial_guess_2019.append(y_pred_extrapolate_int[1])
-                # initial_guess_2020.append(y_pred_extrapolate_int[2])
-            # print(pd.DataFrame(data_full))
-            if(float(row.indeks_2018)>0):
-                print("masuk 2018")        
-                model.create_bulk(row.id,'2018',data_full["2018"])
-            if(float(row.indeks_2019)>0):
-                print("masuk 2019")   
-                model.create_bulk(row.id,'2019',data_full["2019"])
-            if(float(row.indeks_2020)>0):
-                print("masuk 2020")   
-                model.create_bulk(row.id,'2020',data_full["2020"])
-            # initial_guess_2018_tahap2=[]
-            # initial_guess_2019_tahap2=[]
-            # initial_guess_2020_tahap2=[]
-            # dianggap dr prediksi sebelumnya indikator 1 benar
-            # for index_in, row_in in data_indikator_2021.iterrows():
-
-            # print(row.indeks_2019,round(objective(initial_guess_2018),2))
-
-
-        #     for indikator in list_indikator:
-        #         indikator_2021=0
-        #         indikator_2022=0
-        #         if(index_2021>0):
-        #             indikator_2021=model.getById(instansi=row.id,indikator=indikator['id'],year=2021)['value']
-        #         if(index_2022>0):
-        #             indikator_2022=model.getById(instansi=row.id,indikator=indikator['id'],year=2021)['value']
-        #         if(indikator_2021==0):indikator_2021=indikator_2022
-        #         if(indikator_2022==0):indikator_2022=indikator_2021
-        #         data_indikator_2021.append(indikator_2021)
-        #         data_indikator_2022.append(indikator_2022)
-        #         y_train = np.array([indikator_2021,indikator_2022])
-        #         # Melatih model regresi linier
-        #         model_regresi = LinearRegression()
-        #         model_regresi.fit(X_train, y_train)
-        #         # # Membuat prediksi untuk data di luar rentang pelatihan
-        #         X_extrapolate = np.array([row.indeks_2018,row.indeks_2019,row.indeks_2020]).reshape(-1, 1)
-        #         y_pred_extrapolate = model_regresi.predict(X_extrapolate)
-        #         # Modifikasi hasil prediksi menjadi bilangan bulat dalam rentang 0-5
-        #         y_pred_extrapolate_int = np.clip(np.round(y_pred_extrapolate), 0, 5)
-        #         initial_guess.append(y_pred_extrapolate_int)
-
-        # print(y_pred_extrapolate_int)
-         
-# Membuat data untuk pelatihan
-# X_train = np.array([1.98,2.92]).reshape(-1, 1)
-# y_train = np.array([1,3])
-
-# # Melatih model regresi linier
-# model = LinearRegression()
-# model.fit(X_train, y_train)
-
-# # Membuat prediksi untuk data di luar rentang pelatihan
-# X_extrapolate = np.array([2.09,1.89,2.57]).reshape(-1, 1)
-# y_pred_extrapolate = model.predict(X_extrapolate)
-
-# # Modifikasi hasil prediksi menjadi bilangan bulat dalam rentang 0-5
-# y_pred_extrapolate_int = np.clip(np.round(y_pred_extrapolate), 0, 5)
-
-# # Menampilkan hasil prediksi
-# for x, y_pred in zip(X_extrapolate, y_pred_extrapolate_int):
-#     print(f'Input: {x[0]}, Prediksi (bulat): {int(y_pred)}')
-
-# Plot hasil
-# plt.scatter(X_train, y_train, label='Data Pelatihan')
-# plt.plot(X_train, model.predict(X_train), color='blue', label='Model Regresi Linier')
-# plt.scatter(X_extrapolate, y_pred_extrapolate_int, color='red', label='Prediksi Ekstrapolasi (bulat)')
-# plt.xlabel('Variabel Independen (X)')
-# plt.ylabel('Variabel Dependen (Y)')
-# plt.legend()
-#  plt.show()
+            # # print(indeks_2020,index_2019,indeks_2020>index_2019)
+            # for i in range(47):
+            #     indikator_2020=data_2020[i]
+            #     indikator_2021=float(data_indikator_2021.loc[i].val)
+            #     indikator_2019=[];
+                
+            #     if(indeks_2020>index_2019):
+            #         # print("lebih besar")
+            #         #  naik indeks
+            #         if(indikator_2020>indikator_2021):
+            #             # indikator naik
+            #             indikator_2019.append(indikator_2020+1);
+            #         elif(indikator_2020==indikator_2021):
+            #             indikator_2019.append(indikator_2020);
+            #             indikator_2019.append(indikator_2020+1);
+            #         elif(indikator_2020<indikator_2021):
+            #             indikator_2019.append(indikator_2020-1);
+            #             indikator_2019.append(indikator_2020+1);
+            #     elif(indeks_2020==index_2019):
+            #         # print("sama")
+            #         indikator_2019.append(indikator_2020);
+            #     else:
+            #         # print("lebih kecil")
+            #         if(indikator_2020<indikator_2021):
+            #             # indikator naik
+            #             indikator_2019.append(indikator_2020-1);
+            #         elif(indikator_2020==indikator_2021):
+            #             indikator_2019.append(indikator_2020-1);
+            #             indikator_2019.append(indikator_2020);
+            #         elif(indikator_2020>indikator_2021):
+            #             indikator_2019.append(indikator_2020-1);
+            #             indikator_2019.append(indikator_2020+1);
+            #     print(indikator_2021,indikator_2020,indikator_2019[0])
+            #     data_2019.append(indikator_2019[0])
+            #     pilihan_data_2019.append([indikator_2019,0])
+            # cnt=0
+            # while(index_2020>objective(data_2019) and cnt<47):
+            #     # print(data_2019[cnt])
+            #     if(len(pilihan_data_2019[cnt][0])>1):
+            #         data_2019[cnt]=pilihan_data_2019[cnt][0][1]
+            #         if(index_2020<objective(data_2019)):data_2019[cnt]=pilihan_data_2019[cnt][0][0]
+            #     # print(cnt,data_2019[cnt])
+            #     cnt+=1
+            # indeks_2019=round(objective(data_2019),2)
+            indeks_2018=0
+            indeks_2019=0
+            print("indeks_obj :",indeks_2018,indeks_2019,indeks_2020,index_2021,index_2022)
+           
