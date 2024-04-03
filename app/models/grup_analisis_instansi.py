be25e39add929b67f4e6a69d4346ecb1002d46cd
ElaineAndrianaHosni <6182001045@student.unpar.ac.id>
2024-04-03 21:41:03 +0700

    grup_analisis

--- app/models/grup_analisis_instansi.py
+++ app/models/grup_analisis_instansi.py
@@ -1,19 +1,21 @@
 from app.utils.database import Database
 from datetime import datetime
-db=Database()
+
 class grup_analisis_instansiModel:
     table_name="analisis_grup"
     def getAll(self):
+        db=Database()
         query="SELECT * FROM "+self.table_name;
         cur= db.execute_query(query)
         result=cur.fetchall()
         data=[]
         for row in result:
             data.append({"id":row[0],"nama":row[1], "grup":row[2]})
-        # cur.close()
+        cur.close()
         db.close()
         return data
     def getById(self, id):
+        db=Database()
         query="SELECT * FROM "+self.table_name 
         query+=" WHERE id=%s"
         cur= db.execute_query(query,(id,))
@@ -25,6 +27,7 @@ class grup_analisis_instansiModel:
         db.close()
         return data
     def getAllByGrup(self,grup):
+        db=Database()
         query="SELECT * FROM "+self.table_name;
         query+=" WHERE grup=%s"
         cur= db.execute_query(query,(grup,))
