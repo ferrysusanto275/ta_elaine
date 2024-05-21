30dd5e5120602fe2bc047b65785ff786ad1d091e
Ferry Susanto <ferrysusanto275@gmail.com>
2024-05-21 12:58:55 +0700

    pca aglo done

--- app/static/js/pca_agglo.js
+++ app/static/js/pca_agglo.js
@@ -1,5 +1,6 @@
 const year_api = base_api_url + "isi/year";
 const pca_api = base_api_url + "isi/pca_agglo";
+const area_api = base_api_url + "area";
 const year_cb_filter = document.getElementById("year_cb_filter");
 const cari_cb_filter = document.getElementById("cari_cb_filter");
 const linkage_cb_filter = document.getElementById("linkage_cb_filter");
@@ -22,7 +23,35 @@ const cek_year = () => {
 
           year_cb_filter.appendChild(option);
         });
+        cek_area();
+      }
+    })
+    .catch((error) => {
+      console.error("Ada kesalahan:", error);
+    });
+};
+const cek_area = () => {
+  fetch(area_api)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json(); // Ganti dengan response.text() jika Anda mengharapkan data dalam bentuk teks
+    })
+    .then((data) => {
+      if (data.length > 0) {
+        data.forEach((element, i) => {
+          let option = document.createElement("option");
+          option.value = element.id;
+          option.textContent = element.nama;
+          let option_filter = option.cloneNode(true);
+          // area_cb.appendChild(option);
+          cari_cb_filter.appendChild(option_filter);
+        });
         load_img();
+        // load_data();
+      } else {
+        location.href = "area";
       }
     })
     .catch((error) => {
@@ -31,7 +60,13 @@ const cek_year = () => {
 };
 const load_img = () => {
   tampil_perbandingan.innerHTML = "";
-  tampil_perbandingan.appendChild(create_pca(year_cb_filter.value, cari_cb_filter.value));
+  tampil_perbandingan.appendChild(
+    create_pca(
+      year_cb_filter.value,
+      cari_cb_filter.value,
+      linkage_cb_filter.value
+    )
+  );
 };
 const create_link = (year, cari, linkage) => {
   return `${pca_api}/${year}/${cari}/${linkage}`;
