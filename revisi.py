c90a7bf13acd96473a92a299b6dbb9a9001e887a
Ferry Susanto <ferrysusanto275@gmail.com>
2024-06-06 23:20:58 +0700

    domain 2020 done di revisi

--- revisi.py
+++ revisi.py
@@ -2,7 +2,12 @@ import numpy as np
 import pandas as pd
 import decimal
 from app.models.isi import isiModel
-
+def hitungIndex(domain1,domain2,domain3,domain4):
+    return round((domain1*(13/100))+(domain2*(25/100))+(domain3*(16.5/100))+(domain4*(45.5/100)),2)
+def hitungDomain1(aspek1):
+    return aspek1*13/13
+def hitungAspek1(i1,i2,i3,i4,i5,i6,i7,i8,i9,i10):
+    return (i1+i2+i3+i4+i5+i6+i7+i8+i9+i10)*(1.3/13)
 isi=isiModel()
 df_allValue=isi.getAllValue()
 df = pd.read_csv('Data CSV/Data_lengkap_part_01.csv')
@@ -22,71 +27,68 @@ for index, row in df.iterrows():
         domain3_2022=data_2022['domain3'].values[0]
         domain4_2021=data_2021['domain4'].values[0]
         domain4_2022=data_2022['domain4'].values[0]
-        # cari domain 1
+        # cari domain
         domain1_2020=float(round(domain1_2021,2))
         domain2_2020=float(round(domain2_2021,2))
         domain3_2020=float(round(domain3_2021,2))
         domain4_2020=float(round(domain4_2021,2))
-        print(domain1_2020)
-        if(indeks_2020>indeks_2021 and domain1_2021>domain1_2022):
-            if(domain1_2020<5):
-                domain1_2020=round(domain1_2020+0.01,2)
-        if(indeks_2020>indeks_2021 and domain1_2021<domain1_2022):
-            if(domain1_2020>1):
-                domain1_2020=round(domain1_2020-0.01,2)
-        
-        # if(domain1_2021!=domain1_2022):
-        #     indeks = np.array([data_2021['indeks'].values[0], data_2022['indeks'].values[0]])
-        #     indeks = [float(d) for d in indeks]
-        #     domain1 = np.array([data_2021['domain1'].values[0], data_2022['domain1'].values[0]])
-        #     domain1 = [float(d) for d in domain1]
-        #     domain2 = np.array([data_2021['domain2'].values[0], data_2022['domain2'].values[0]])
-        #     domain2 = [float(d) for d in domain2]
-        #     domain3 = np.array([data_2021['domain3'].values[0], data_2022['domain3'].values[0]])
-        #     domain3 = [float(d) for d in domain3]
-        #     domain4 = np.array([data_2021['domain4'].values[0], data_2022['domain4'].values[0]])
-        #     domain4 = [float(d) for d in domain4]
-        #     m1, b1 = np.polyfit(indeks, domain1, 1)  # Use decimal.Decimal for high precision
-        #     m2, b2 = np.polyfit(indeks, domain2, 1)  # Use decimal.Decimal for high precision
-        #     m3, b3  = np.polyfit(indeks, domain3, 1)  # Use decimal.Decimal for high precision
-        #     m4, b4 = np.polyfit(indeks, domain4, 1)  # Use decimal.Decimal for high precision
+        index_res_2020=hitungIndex(domain1_2020,domain2_2020,domain3_2020,domain4_2020)
+        flagDomain1=True
+        flagDomain2=True
+        flagDomain3=True
+        flagDomain4=True
+        while(index_res_2020<indeks_2020-0.013):
 
-            # if(indeks_2020>indeks_2021 and domain1_2021>domain1_2022):
-            # Extrapolated value for indeks 3.37
-            # domain1_2020 = round(m1 * indeks_2020 + b1,2)
-            # domain2_2020 = round(m2 * indeks_2020 + b2,2)
-            # domain3_2020 = round(m3 * indeks_2020 + b3,2)
-            # domain4_2020 = round(m4 * indeks_2020 + b4,2)
-        # domain1_2020=round(domain1_2020+0.01,2)
-        index_res_2020=round(((domain1_2020*13)+(domain2_2020*25)+(domain3_2020*16.50)+(domain4_2020*45.50))/100,2)
+            if(domain1_2020<indeks_2020 and flagDomain1):
+                # print("masuk domain1")
+                # bobot terkecil domain 1 = 
+                domain1_2020=round(domain1_2020+0.1,2)
+                flagDomain1=False
+            elif(domain2_2020<indeks_2020 and flagDomain2):
+                # print("masuk domain2")
+                domain2_2020=round(domain2_2020+0.2,2)
+                flagDomain2=False
+            elif(domain3_2020<indeks_2020  and flagDomain3):
+                # print("masuk domain3")
+                domain3_2020=round(domain3_2020+0.27,2)
+                flagDomain3=False
+            elif(domain4_2020<indeks_2020 and flagDomain4):
+                # print("masuk domain4")
+                domain4_2020=round(domain4_2020+0.39,2)
+                flagDomain4=False
+            else:
+                # print("reset")
+                flagDomain1=True
+                flagDomain2=True
+                flagDomain3=True
+                flagDomain4=True
+            index_res_2020=hitungIndex(domain1_2020,domain2_2020,domain3_2020,domain4_2020)
+        while(index_res_2020>indeks_2020+0.013):
+
+            if(domain1_2020>1 and flagDomain1):
+                # print("masuk domain1")
+                # bobot terkecil domain 1 = 
+                domain1_2020=round(domain1_2020-0.1,2)
+                flagDomain1=False
+            elif(domain2_2020>1 and flagDomain2):
+                # print("masuk domain2")
+                domain2_2020=round(domain2_2020-0.2,2)
+                flagDomain2=False
+            elif(domain3_2020>1  and flagDomain3):
+                # print("masuk domain3")
+                domain3_2020=round(domain3_2020-0.27,2)
+                flagDomain3=False
+            elif(domain4_2020+1 and flagDomain4):
+                # print("masuk domain4")
+                domain4_2020=round(domain4_2020-0.39,2)
+                flagDomain4=False
+            else:
+                # print("reset")
+                flagDomain1=True
+                flagDomain2=True
+                flagDomain3=True
+                flagDomain4=True
+            index_res_2020=hitungIndex(domain1_2020,domain2_2020,domain3_2020,domain4_2020)
         print(indeks_2020,domain1_2020,domain2_2020,domain3_2020,domain4_2020,index_res_2020)
-            # m, b = np.polyfit(indeks, domain1, 1)
-            # domain1_2020 = m * indeks_2020 + b
-            # print(domain1_2020)
-        #     print(row.id)
-        #     print('indeks')
-        #     print(indeks_2020)
-        #     print(data_2021['indeks'].values[0])
-        #     print(data_2022['indeks'].values[0])
-        #     print("domain1")
-        #     print(data_2021['domain1'].values[0])
-        #     print(data_2022['domain1'].values[0])
-        #     domain1_2020=domain1_2021+0.01
-
-
-    # else:
-        # print(row.instansi)
-    # if(indeks_target>data_2021["indeks"].values[0])
-    
-    # print(indeks_target)
-# Data points
-# indeks = np.array([2.90, 3.12])
-# domain1 = np.array([3.10, 2.70])
-
-# Assuming a linear relationship (might not be accurate)
-# m, b = np.polyfit(indeks, domain1, 1)
-
-# Extrapolated value for indeks 3.68
-# extrapolated_domain1 = m * 3.68 + b
-
-# Disclaimer
+        # break
+        
